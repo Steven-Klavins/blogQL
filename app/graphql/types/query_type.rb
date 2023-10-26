ce53531a785b132f38f56a349578b1083429e0a6
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-26 14:30:06 +0100

    include root level fields for user, post & comment

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,11 +21,28 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :user, Types::UserType, null: true, description: "Returns one instance of a user." do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.where(id: id).first
+    end
+
+    field :post, Types::PostType, null: true, description: "Returns one instance of a post." do
+      argument :id, ID, required: true
+    end
+
+    def post(id:)
+      Post.where(id: id).first
+    end
+
+    field :comment, Types::CommentType, null: true, description: "Returns one instance of a comment." do
+      argument :id, ID, required: true
+    end
+
+    def comment(id:)
+      Comment.where(id: id).first
     end
   end
 end
