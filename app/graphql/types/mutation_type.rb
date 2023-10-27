d5810f34aa144ec98c26b722d8bcbb141c5feeb9
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-27 18:03:40 +0100

    added crud actions for user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -48,5 +48,28 @@ module Types
     def delete_comment(id:)
       Comment.where(id: id).destroy_all
     end
+
+    # =================================================
+    #                    USERS
+    # =================================================
+
+    field :create_user, Types::UserType, mutation: Mutations::CreateUser
+
+    field :update_user, Boolean, null: false, description: "Update a user." do
+      argument :user, Types::UserInputType, required: true
+    end
+
+    def update_user(user:)
+      existing = User.where(id: user.id).first
+      existing&.update(user.to_h)
+    end
+
+    field :delete_user, Boolean, null: false, description: "Delete a user." do
+      argument :id, ID, required: true
+    end
+
+    def delete_user(id:)
+      User.where(id: id).destroy_all
+    end
   end
 end
