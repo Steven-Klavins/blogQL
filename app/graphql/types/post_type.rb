7f117d02bf7b6e64aebef1d010d78654836e7b55
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-27 17:10:52 +0100

    added post input type and errors

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -7,6 +7,7 @@ class Types::PostType < Types::BaseObject
   field :body, String, null: false
   field :comments, [Types::CommentType], null: false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :errors, [Types::ErrorType], null: true
 
   def author
     user = User.where(object.user_id)
@@ -16,4 +17,17 @@ class Types::PostType < Types::BaseObject
   def comments
     object.comments
   end
+
+  def errors
+    object.errors.map {|e| {field_name: e.attribute, error: object.errors[e.attribute]}}
+  end
+end
+
+class Types::PostInputType < GraphQL::Schema::InputObject
+  graphql_name "PostInputType"
+  description "The attributes required to create/update an post."
+
+  argument :id, ID, required: false
+  argument :user_id, Integer, required: true
+  argument :body, String, required: true
 end
