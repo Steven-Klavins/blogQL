1fca35b519eaab92f15aeeadc11c6b77632864ca
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-27 17:40:29 +0100

    added comment input type

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -6,3 +6,12 @@ class Types::CommentType < Types::BaseObject
   field :body, String, null: false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 end
+
+class Types::CommentInputType < GraphQL::Schema::InputObject
+  graphql_name "CommentInputType"
+  description "The attributes required to create/update a comment."
+
+  argument :id, ID, required: false
+  argument :post_id, Integer, required: false
+  argument :body, String, required: true
+end
